2e0d6c13af652b64765a6dc0d8b8db0ad1384f4c
parkero2 <parkero2@stu.otc.school.nz>
2022-12-22 10:21:50 +1300

    Did some stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from genericpath import isfile
 import os
 import shutil
 import git
+from git import Repo
 
 shw, xhw, sbk = [], [], []
 
@@ -19,22 +20,38 @@ for filename in os.listdir(MAGICQ_DIR):
 
 print("Found " + str(len(shw)) + " .shw files, " + str(len(xhw)) + " .xhw files, and " + str(len(sbk)) + " .sbk files")
 
+try:
+    repo = Repo(os.path.join(os.getcwd(), "dirs", ".git"))
+except:
+    repo = Repo.init(os.path.join(os.getcwd(), "dirs"))
+
 for item in shw:
+    print("processing " + item)
     if not (os.path.exists(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))):
         os.mkdir(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
+        repo.git.add(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item), update=True)
     else:
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item))
 for item in xhw:
+    print("processing " + item)
     if not (os.path.exists(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))):
         os.mkdir(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
+        repo.git.add(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item), update=True)
     else:
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item))
 for item in sbk:
+    print("processing " + item)
     if not (os.path.exists(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))):
         os.mkdir(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4]))
+        repo.git.add(os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item), update=True)
     else:
         shutil.copy2(os.path.join(MAGICQ_DIR, item), os.path.join(os.getcwd(), "dirs", item[0:len(item)-4], item))
 
+repo.git.commit(m="Committing all files")
+my_new_branch = repo.create_head('my_new_branch')
+my_new_branch.checkout()
+origin = repo.create_remote('origin', "https://parkero2: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@github.com/OTC-PAC/Backup.git")
+repo.git.push("--set-upstream", origin, repo.head.ref)
